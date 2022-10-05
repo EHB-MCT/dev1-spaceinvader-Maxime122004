342f35a88fa7cb05fa7c9f5dc5d5259dcad4aa5b
Maxime Bastien <maxime.bastien@student.ehb.be>
2022-10-05 22:13:22 +0200

    Opdracht Space Invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,74 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+drawInvader();
+
+function drawInvader(){
+
+    //background//
+    context.beginPath();
+    context.rect(25, 25, 300, 300);
+    context.fill();
+
+    //bovenste deel//
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(50, 100, 50, 50);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(100, 100, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(100, 50, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(150, 50, 50, 50);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(200, 50, 50, 50);
+    context.fill(); 
+
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(200, 100, 50, 50);
+    context.fill(); 
+    
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(250, 100, 50, 50);
+    context.fill(); 
+
+    //onderste deel//
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(150, 200, 50, 50);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(100, 250, 50, 50);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(150, 250, 50, 50);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = "#00ff00";
+    context.rect(200, 250, 50, 50);
+    context.fill();
+
+}   
